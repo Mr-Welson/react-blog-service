66affd6e05e2bdf28f1380f0d5f040c7968ed799
Mr.Welson <iwelson@foxmail.com>
2022-03-10 17:03:46 +0800

    添加管理端接口

--- app/controller/admin/index.js
+++ app/controller/admin/index.js
@@ -21,6 +21,7 @@ class IndexController extends Controller {
     };
   }
 
+  // 登录
   async login() {
     const { ctx, app } = this;
     const { username, password } = ctx.request.body;
@@ -38,9 +39,86 @@ class IndexController extends Controller {
       }
       const openId = new Date().getTime();
       ctx.session.openId = { openId };
-      ctx.body = this.handleSuccessResult(data[0]);
+      ctx.body = this.handleSuccessResult(openId);
     } catch (error) {
       ctx.body = this.handleErrorResult(error, {});
+    }
+  }
+
+  // 获取文章分类
+  async getTypeList() {
+    const data = await this.app.mysql.select('type');
+    this.ctx.body = this.handleSuccessResult(data);
+  }
+
+  // 获取文章列表
+  async queryArticleList() {
+    const { ctx, app } = this;
+    const sql = `
+      SELECT article.id as id,
+      article.title as title,
+      article.introduce as introduce,
+      article.create_time as createTime,
+      article.view_count as viewCount ,
+      type.name as typeName 
+      FROM article LEFT JOIN type ON article.type_id = type.Id 
+      ORDER BY article.id DESC 
+    `;
+    try {
+      const data = await app.mysql.query(sql);
+      ctx.body = this.handleSuccessResult(data);
+    } catch (error) {
+      ctx.body = this.handleErrorResult(error, []);
+    }
+  }
+
+  // 添加文章
+  async addArticle() {
+    const { ctx, app } = this;
+    try {
+      const articleInfo = ctx.request.body;
+      const { typeId, createTime, viewCount, ...rest } = articleInfo;
+      const insertInfo = {
+        type_id: typeId,
+        create_time: createTime,
+        view_count: viewCount,
+        ...rest,
+      };
+      const result = await app.mysql.insert('article', insertInfo);
+      const { insertId, affectedRows } = result;
+      const insertSuccess = affectedRows === 1;
+      if (insertSuccess) {
+        ctx.body = this.handleSuccessResult({ id: insertId });
+      } else {
+        ctx.body = this.handleErrorResult(result);
+      }
+    } catch (error) {
+      ctx.body = this.handleErrorResult(error);
+    }
+  }
+
+  // 更新文章
+  async updateArticle() {
+    const { ctx, app } = this;
+    try {
+      const articleInfo = ctx.request.body;
+      const { typeId, createTime, viewCount, ...rest } = articleInfo;
+      const insertInfo = {
+        type_id: typeId,
+        create_time: createTime,
+        view_count: viewCount,
+        ...rest,
+      };
+      const result = await app.mysql.update('article', insertInfo);
+      const { affectedRows } = result;
+      const insertSuccess = affectedRows === 1;
+      if (insertSuccess) {
+        ctx.body = this.handleSuccessResult();
+      } else {
+        ctx.body = this.handleErrorResult(result);
+      }
+    } catch (error) {
+      ctx.body = this.handleErrorResult(error);
     }
   }
 }
--- app/router/admin.js
+++ app/router/admin.js
@@ -3,7 +3,12 @@
 
 module.exports = (app) => {
   const { router, controller } = app;
+  const adminAuth = app.middleware.adminauth();
   const { index } = controller.admin;
   router.get('/admin/index', index.index);
   router.post('/admin/login', index.login);
+  router.get('/admin/getTypeList', adminAuth, index.getTypeList);
+  router.get('/admin/queryArticleList', adminAuth, index.queryArticleList);
+  router.post('/admin/addArticle', adminAuth, index.addArticle);
+  router.post('/admin/updateArticle', adminAuth, index.updateArticle);
 };
